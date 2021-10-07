7ea0b2814fc82e68c4b2c0bcfd9970594b9ef2e0
drajo <drajo@DESKTOP-LSTFBT7>
2021-10-07 09:54:54 -0700

    Add project files.

---
+++ Week 3 Activity 7/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Week_3_Activity_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter # of terms:";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(201, 54);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(186, 29);
+            this.inputBox.TabIndex = 1;
+            // 
+            // submitButton
+            // 
+            this.submitButton.BackColor = System.Drawing.Color.LightSlateGray;
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(167, 116);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(106, 39);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Calculate";
+            this.submitButton.UseVisualStyleBackColor = false;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(303, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Approximate value of pi after _____ terms";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(93, 234);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(18, 20);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(462, 372);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Aprroximate Pi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Week 3 Activity 7/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week_3_Activity_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            int x; // user input variable
+
+            // confirms the input is a valid integer greater than 0
+            if (int.TryParse(inputBox.Text, out x) && x > 0) {
+
+                double pi = 0; // output variable
+                int y = 1; // formula variable
+
+                // iterrates through the for loop x number of times using i as a count
+                for (int i = 0; i < x; i++)
+                {
+                    if ((i % 2) == 0) // checks if i is even or odd
+                    {
+                        pi = pi + ((double) 4 / y); // on even iterations the number is added
+                    }
+                    else
+                    {
+                        pi = pi - ((double) 4 / y); // on odd iterations the number is subtracted
+                    }
+                    y = y + 2; // y is incremented by 2 each time
+                }
+
+                label2.Text = "Approximate value of pi after " + inputBox.Text + " terms"; // adds the user input to the ouput text
+                outputLabel.Text = "= " + pi.ToString(); // prints the value value of pi
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid positive integer"); // if the try parse fails this error message is shown
+            }
+        }
+    }
+}
